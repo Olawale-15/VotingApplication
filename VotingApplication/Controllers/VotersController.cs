8195a0e27e0400d337c1b083ccf5307ff172cd23
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-12-31 13:28:17 +0100

    voters controller added

---
+++ VotingApplication/Controllers/VotersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VotingApplication.DTOs.VotersDTO;
+using VotingApplication.Services.Interface;
+
+namespace VotingApplication.Controllers
+{
+    public class VotersController(IVotersService voters) : Controller
+    {
+        public IActionResult RegisterVoters()
+        {
+            return View();
+        }
+
+        [HttpPost("Register-voter")]
+        public IActionResult RegisterVoters(RegisterVotersRequestModel requestModel)
+        {
+            voters.RegisterVoter(requestModel);
+            return View();
+        }
+    }
+}
